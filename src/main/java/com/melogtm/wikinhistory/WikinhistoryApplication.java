0199cd8406a0a969414ebcd984a34fca993652e6
melogtm <gabrielcursos@proton.me>
2024-07-26 12:25:51 -0300

    feat: war packing

--- src/main/java/com/melogtm/wikinhistory/WikinhistoryApplication.java
+++ src/main/java/com/melogtm/wikinhistory/WikinhistoryApplication.java
@@ -2,9 +2,10 @@ package com.melogtm.wikinhistory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WikinhistoryApplication {
+public class WikinhistoryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WikinhistoryApplication.class, args);
